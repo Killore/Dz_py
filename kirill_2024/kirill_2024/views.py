ca4d5a664904c6babbb60cbff56d58bbc842f105
Killore <gaiduk_kirill@mail.ru>
2024-02-07 19:37:28 +0300

    Dz

--- kirill_2024/kirill_2024/views.py
+++ kirill_2024/kirill_2024/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html', {'gretting': 'Reverse!'})
+
+
+def reverse(request):
+    user_text = request.GET['username']
+    reverse = user_text[::-1]
+    return render(request, 'reverse.html', {'word': reverse})
